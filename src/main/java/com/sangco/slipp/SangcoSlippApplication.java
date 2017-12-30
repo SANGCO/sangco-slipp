8b456473f9819a2771ab269cbd8e37e7ba58cc68
SANGCO <nsg1213@gmail.com>
2017-12-30 11:17:58 +0900

    Initial Commit

---
+++ src/main/java/com/sangco/slipp/SangcoSlippApplication.java
@@ -0,0 +1,12 @@
+package com.sangco.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangcoSlippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangcoSlippApplication.class, args);
+	}
+}
