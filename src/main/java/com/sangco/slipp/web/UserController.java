edd747ccc3a8cb59ef9ec08c31cfc9e5be35625f
SANGCO <nsg1213@gmail.com>
2018-01-04 20:26:43 +0900

    Writing and Reply Function done

--- src/main/java/com/sangco/slipp/web/UserController.java
+++ src/main/java/com/sangco/slipp/web/UserController.java
@@ -33,18 +33,18 @@ public class UserController {
 			System.out.println("Login Failure!");
 			return "redirect:/users/loginForm";
 		}
-		if (!password.equals(user.getPassword())) {
+		if (!user.matchPassword(password)) {
 			System.out.println("Login Failure!");
 			return "redirect:/users/loginForm";
 		}
 		System.out.println("Login Success!");
-		session.setAttribute("sessionedUser", user);
+		session.setAttribute(HttpSessionUtils.USER_SESSION_KEY, user);
 		return "redirect:/";
 	}
 
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
-		session.removeAttribute("sessionedUser");
+		session.removeAttribute(HttpSessionUtils.USER_SESSION_KEY);
 		return "redirect:/";
 	}
 	
@@ -70,11 +70,11 @@ public class UserController {
 	@GetMapping("/{id}/form")
 	public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
 		Object tempUser = session.getAttribute("sessionedUser");
-		if (tempUser == null) {
+		if (HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-		User sessionedUser = (User)tempUser;
-		if(!id.equals(sessionedUser.getId())) {
+		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+		if(!sessionedUser.matchId(id)) {
 			throw new IllegalStateException("You can't update the another user");
 		}
 		User user = userRepository.findOne(id);
@@ -85,11 +85,11 @@ public class UserController {
 	@PutMapping("/{id}")
 	public String update(@PathVariable Long id, User updatedUser, HttpSession session) {
 		Object tempUser = session.getAttribute("sessionedUser");
-		if (tempUser == null) {
+		if (HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-		User sessionedUser = (User)tempUser;
-		if(!id.equals(sessionedUser.getId())) {
+		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+		if(!sessionedUser.matchId(id)) {
 			throw new IllegalStateException("You can't update the another user");
 		}
 		User user = userRepository.findOne(id);
