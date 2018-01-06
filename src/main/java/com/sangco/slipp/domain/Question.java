ff1253d8782b38202fdc855dc50edd37a3dfc5c9
SANGCO <nsg1213@gmail.com>
2018-01-06 15:06:35 +0900

    Add Reply

--- src/main/java/com/sangco/slipp/domain/Question.java
+++ src/main/java/com/sangco/slipp/domain/Question.java
@@ -1,8 +1,18 @@
 package com.sangco.slipp.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 
 @Entity
 public class Question {
@@ -10,17 +20,41 @@ public class Question {
 	@GeneratedValue
 	private Long id;
 
-	private String writer;
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+	private User writer;
 	private String title;
+	@Lob
 	private String contents;
+	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy="question")
+	@OrderBy("id ASC")
+	private List<Answer> answers;
 
 	public Question() {
 	}
 
-	public Question(String writer, String title, String contents) {
-		super();
+	public Question(User writer, String title, String contents) {
 		this.writer = writer;
 		this.title = title;
 		this.contents = contents;
+		this.createDate = LocalDateTime.now();
+	}
+	
+	public String getFormatterdCreateDate() {
+		if (createDate == null) {
+			return "";
+		}
+		return createDate.format(DateTimeFormatter.ofPattern("yyy.MM.dd HH:mm:ss"));
+	}
+
+	public void update(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
+	
+	public boolean isSameWriter(User loginUser) {
+		return this.writer.equals(loginUser);
 	}
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,6 @@
-INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('nsg1213', 'test', '상코', 'nsg1213@gmail.com');
-INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('bok1213', 'test', '복코', 'bok1213@gmail.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (1, 'nsg1213', 'test', '상코', 'nsg1213@gmail.com');
+INSERT INTO USER (ID, USER_ID, PASSWORD, NAME, EMAIL) VALUES (2, 'bok1213', 'test', '복코', 'bok1213@gmail.com');
+
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date) VALUES (1, 1, '타이틀1', '컨텐츠1', CURRENT_TIMESTAMP());
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date) VALUES (2, 2, '타이틀2', '컨텐츠2', CURRENT_TIMESTAMP());
+
